25099451113f2609eb6b50807291f13a032c5d87
angelo-francisco <ics20080729@gmail.com>
2024-07-15 22:48:43 +0100

    Project finished

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+# coding utf-8
+
+import os
+import json
 from io import StringIO
 from time import sleep
 from bs4 import BeautifulSoup
@@ -6,9 +10,12 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from pandas import read_html
 
-
+BASE_DIR = os.path.abspath(os.curdir)
+OUT_PATH = os.path.join(BASE_DIR, 'src/out/fifa.json')
 URL = "https://www.fifa.com/en/tournaments/mens/worldcup/canadamexicousa2026/qualifiers/conmebol/standings"
 
+# options = Options()
+# options.headless = True
 driver = webdriver.Firefox()
 driver.get(URL)
 
@@ -20,13 +27,19 @@ element = driver.find_element(
 )
 html_content = element.get_attribute("outerHTML")
 
-# print(html_content)
-
 soup = BeautifulSoup(html_content, "html.parser")
 table = soup.find(name="table")
 
-df_full = read_html(StringIO(str(table)))[0]
-df = df_full[["Team", "Pts"]]
-df.columns = ["TEAM", "PTS"]
+df_full = read_html(StringIO(str(table)))[0].head(10)
+df = df_full[["Team", "P", "W", "D", "L", "Pts"]]
+df.columns = ["SELECION",  "P", "W", "D", "L", "PTS"]
 
-print(df)
+driver.quit()
+
+fifaStadings = df.to_dict('records')
+
+js = json.dumps(fifaStadings)
+
+with open(OUT_PATH, 'w') as file:
+    file.write(js)
+
